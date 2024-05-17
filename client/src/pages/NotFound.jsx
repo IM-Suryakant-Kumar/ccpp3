04f9271032dd2683f58770b7acfc151e1b7def9f
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-05-17 08:20:21 +0530

    feat: added NotFound page

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <h2 className="text-red-500 text-lg md:text-3xl">404 - PAGE NOT FOUND!</h2>
+    </main>
+  )
+}
